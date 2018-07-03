2e9e6c27e7ba9947b7c0c11c241c23b1b6c40ae8
Tamilarasanmca <39939330+Tamilarasanmca@users.noreply.github.com>
2018-07-03 13:57:01 +0530

    Update DBOperations.java
    insert,update,delete and view operations

--- JDBC/DBOperations.java
+++ JDBC/DBOperations.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import com.mysql.jdbc.PreparedStatement;
 
 public class DBOperations {
